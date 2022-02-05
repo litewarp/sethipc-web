8a0dc5939f0e8f9c48db6fd050ec9947f0556bfc
Nick Sethi <nicholas.sethi@gmail.com>
2022-02-04 18:13:36 -0800

    configure stitches

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,18 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "next-themes";
+import { darkTheme, globalStyles } from "../stitches.config";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  globalStyles();
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="system"
+      value={{ dark: darkTheme.className, light: "light" }}
+    >
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
